2a5b9792a270157fc637df1748aac84fef0d6bc7
Md Danish Raza <md.danish0raza@gmail.com>
2025-09-22 21:41:07 +0530

    landing page and styled comps

---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+function Landing() {
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobster logo" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Job hunting is stressful enough. Spreadsheets get messy, emails get
+            lost, and opportunities slip through the cracks. <b>Jobster</b>{" "}
+            gives you a single, elegant dashboard to manage every step of your
+            journey—from first click to final offer.{" "}
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+}
+
+export default Landing;
