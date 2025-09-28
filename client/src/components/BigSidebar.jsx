e41217d1eef5bd8e26d878dd9372b627b2a5636f
Md Danish Raza <md.danish0raza@gmail.com>
2025-09-28 13:33:53 +0530

    sidebar small and big

--- client/src/components/BigSidebar.jsx
+++ client/src/components/BigSidebar.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/BigSidebar";
+import { useSelector, useDispatch } from "react-redux";
+import links from "../utils/links";
+import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
 function BigSidebar() {
-  return <Wrapper>bigsidebar</Wrapper>;
+  const isSidebarOpen = useSelector((state) => state.user.isSidebarOpen);
+  const dispatch = useDispatch();
+  return (
+    <Wrapper>
+      <div
+        className={
+          isSidebarOpen
+            ? "sidebar-container "
+            : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">
+            {links.map((link) => {
+              const { text, path, id, icon } = link;
+
+              return (
+                <NavLink
+                  to={path}
+                  key={id}
+                  onClick={() => dispatch(toggleSidebar())}
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link"
+                  }
+                >
+                  <span className="icon">{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 }
 
 export default BigSidebar;
--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -1,8 +1,50 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/SmallSidebar";
-
+import { FaTimes } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleSidebar } from "../state/features/userSlice";
+import links from "../utils/links.jsx";
 function SmallSidebar() {
-  return <Wrapper>smallsidebar</Wrapper>;
+  const isSidebarOpen = useSelector((state) => state.user.isSidebarOpen);
+  const dispatch = useDispatch();
+  return (
+    <Wrapper>
+      <div className={`sidebar-container ${isSidebarOpen && "show-sidebar"}`}>
+        <div className="content">
+          <button
+            className="close-btn"
+            onClick={() => dispatch(toggleSidebar())}
+          >
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">
+            {links.map((link) => {
+              const { text, path, id, icon } = link;
+
+              return (
+                <NavLink
+                  to={path}
+                  key={id}
+                  onClick={() => dispatch(toggleSidebar())}
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link"
+                  }
+                >
+                  <span className="icon">{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 }
 
 export default SmallSidebar;
